7353e6d4063abe1c3b54c781702c193fd31eed5a
ghinafarajwati <g.farajwati@yahoo.com>
2022-06-09 14:03:23 +0700

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Faq from "./Pages/Faq/Faq";
 
 function App() {
   return (
-    <div className="App">
-      Welcome Bishapay
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Faq />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
