29676ada7d06872d6170f2eab38a10b8a6ba7daa
ghinafarajwati <g.farajwati@yahoo.com>
2022-06-10 19:10:00 +0700

    faq & component footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Faq from "./Pages/Faq/Faq";
+// import Faq from "./Pages/Faq/Faq";
+import Footer from "./Components/Footer/Footer"
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Faq />}>
+          <Route path="/" element={<Footer />}>
           </Route>
         </Routes>
       </BrowserRouter>
