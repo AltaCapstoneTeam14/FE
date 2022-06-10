1647de347aa8a2acab90b6338430be0b1dd3a486
alfiarisandi <alfyarissandy@gmail.com>
2022-06-10 20:22:07 +0700

    benerin folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-import LandingPage from "./Pages/LandingPage";
-
-function App() {
-  return (
-    <div className="App">
-      
-      <LandingPage />
-    </div>
-=======
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 // import Faq from "./Pages/Faq/Faq";
 import Footer from "./Components/Footer/Footer"
 
@@ -24,7 +13,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
->>>>>>> 29676ada7d06872d6170f2eab38a10b8a6ba7daa
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
-import "bootstrap/dist/css/bootstrap.min.css";
-=======
->>>>>>> 29676ada7d06872d6170f2eab38a10b8a6ba7daa
+
 
 ReactDOM.render(
   <React.StrictMode>
