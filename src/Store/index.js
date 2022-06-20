9b21570f339e0c146f5cc05604a98f86c6b13719
baabyygalll <andiraazzahra19@gmail.com>
2022-06-20 14:42:17 +0700

    remove folder store

--- src/Store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-/** Reducer */
-import userSlice from "../features/userSlice";
-
-export default configureStore({
-  reducer: {
-    counter: userSlice,
-  },
-});
