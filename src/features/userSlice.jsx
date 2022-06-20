d53d72b77d773c219cfccb579c007898ed07ddac
baabyygalll <andiraazzahra19@gmail.com>
2022-06-20 11:47:56 +0700

    add deleteUsers

--- src/features/userSlice.jsx
+++ src/features/userSlice.jsx
@@ -6,18 +6,39 @@ export const getUsers = createAsyncThunk("users/getUsers", async() => {
     return response.data;
 });
 
+
+export const deleteUsers = createAsyncThunk("users/deleteUsers", async (id) => {
+    await fetch("https://jsonplaceholder.typicode.com/users/$(Id)", {
+        method: 'DELETE',
+    })
+    return id;
+
+});
+
 const userEntity = createEntityAdapter({
     selectId: (user) => user.id
 })
 
+
+
 const userSlice = createSlice({
-    name: "user",
-    initialState: userEntity.getInitialState(),
-    extraReducers: {
-        [getUsers.fulfilled]: (state, action) => {
-            userEntity.setAll(state, action.payload);
-        }
-    }
+  name: "user",
+  initialState: userEntity.getInitialState(),
+  extraReducers: {
+    [getUsers.fulfilled]: (state, action) => {
+      userEntity.setAll(state, action.payload);
+    },
+    [deleteUsers.rejected](state) {
+      state.loading = false;
+    },
+    [deleteUsers.pending](state) {
+      state.loading = true;
+    },
+    [deleteUsers.fulfilled](state, { payload: id }) {
+      state.loading = false;
+      createEntityAdapter.removeOne(state, id)
+    },
+  },
 });
 
 export const userSelectors = userEntity.getSelectors(state => state.user)
