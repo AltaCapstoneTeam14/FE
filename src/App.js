b5c91a0a00ace36058767a8178f045e5f77b94a5
ghinafarajwati <g.farajwati@yahoo.com>
2022-06-21 21:22:23 +0700

    styling

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import EditUser from "./Components/User/EditUser"
 import ShowUser from "./Components/User/ShowUser"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Assets/style/main.css';
-import Navbar from "./Components/Navbar/Navbar";
 import LandingPage from './Pages/LandingPage/index'
 import Login from "./Pages/Login/Login";
 import AboutUs from './Pages/AboutUs/AboutUs'
 import Faq from "./Pages/Faq/Faq";
 
-
-
 function App() {
   return (
     <>
       <BrowserRouter>
-      <div className='container'>
+      <div>
         <Routes>
-
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<><Login/></>} />
           <Route path="/aboutus" element={<><AboutUs/></>} />
@@ -26,7 +21,6 @@ function App() {
 
           <Route path='/user' element={<ShowUser />}></Route>
           <Route path='/user/edit/:id' element={<EditUser />}></Route>
-
         </Routes>
       </div>
     </BrowserRouter>
--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -33,9 +33,7 @@ function LandingPage() {
             <Col>
               <div className={style.titleLanding}>
               <h1 className={style.tagline}>
-                Start a Loyalty <br /> Program,{" "}
-                <span style={{ color: "#605CFF" }}>Rewards</span>
-                <br /> Your Business!
+                Start a Loyalty Program, <span>Rewards</span> Your Business!
               </h1>
               <div className={style.paragraphTitle}>
               <p>
@@ -67,19 +65,9 @@ function LandingPage() {
             </Col>
 
             <Col>
-              <p
-                style={{
-                  color: "#C4C4C4",
-                  fontSize: "14px",
-                  fontWeight: "bold",
-                }}
-              >
-                <GoPrimitiveDot className={style.iconDot} />
-                WHO WE ARE{" "}
-              </p>
-              <h2>
-                Upgrade your customer <br /> retention strategy with <br />{" "}
-                Bishapay loyalty solutions
+              <p className={style.landingSection2}> <GoPrimitiveDot className={style.iconDot} /> WHO WE ARE </p>
+              <h2 className={style.landingHeader2}>
+                Upgrade your customer retention strategy with Bishapay loyalty solutions
               </h2>
               <div className={style.paragraphLanding}>
               <p>
@@ -98,18 +86,9 @@ function LandingPage() {
           <Row>
             <Col style={{ marginTop: "7rem" }}>
               <div className={style.titleLanding2}>
-              <p
-                style={{
-                  color: "#C4C4C4",
-                  fontSize: "14px",
-                  fontWeight: "bold",
-                }}
-              >
-                <GoPrimitiveDot className={style.iconDot} />
-                WHAT WE DO
-              </p>
-              <h2>
-                Industry Leading Loyalty <br /> Solutions Made Easy
+              <p className={style.landingSection2}> <GoPrimitiveDot className={style.iconDot} /> WHO WE ARE </p>
+              <h2 className={style.landingHeader3}>
+                Industry Leading Loyalty Solutions Made Easy
               </h2>
               <div className={style.paragraphLanding2}>
               <p>
@@ -131,18 +110,8 @@ function LandingPage() {
         </Container>
 
         {/* OUR PRODUCT SECTION */}
-        <Container
-          style={{ borderBottom: "2px solid #E5E5E5", paddingBottom: "8rem" }}
-        >
-          <h3
-            style={{
-              textAlign: "center",
-              color: "#605CFF",
-              marginTop: "70px",
-            }}
-          >
-            OUR PRODUCTS
-          </h3>
+        <Container style={{ borderBottom: "2px solid #E5E5E5", paddingBottom: "8rem" }}>
+          <h3 className={style.ourProducts}> OUR PRODUCTS </h3>
           <Row>
             <CardGroup>
               <Card className={style.card}>
@@ -221,12 +190,7 @@ function LandingPage() {
             </Col>
 
             <Col className={style.columnLoyaltyPoint}>
-              <h3>
-                Loyalty Point <br />{" "}
-                <span style={{ fontSize: "22px", color: "#605CFF" }}>
-                  By Bishapay
-                </span>
-              </h3>
+              <h3 className={style.loyaltyPoint}> Loyalty Point <span> By Bishapay </span> </h3>
               <p className={style.teksLoyaltyPoint}>
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                 amet sint. Velit officia consequat duis enim velit mollit.
@@ -238,30 +202,24 @@ function LandingPage() {
 
         {/* FAQ SECTION */}
         <Container>
-          <h3 className="text-center fw-bold" style={{marginBottom:"7rem"}}>
-            Frequently asked questions{" "}
-            <BsChevronDown style={{ marginLeft: "2rem" }} />
-          </h3>
+          <div className={style.frequently}>
+            <h3>Frequently asked questions <BsChevronDown style={{ marginLeft: "2rem" }} /></h3>
+          </div>
 
           <div className={style.box}>
-            <img
-              src={news}
-              className={style.news}
-            />
-            <p className={style.newsletter}>Subcribe to our Newsletter</p>
+            <img src={news}/>
+            <p>Subcribe to our Newsletter</p>
 
             <div className={style.form}>
-              <input className={style.inputEmail} type="text" placeholder="Enter your Email Address"></input>
+              <input className={style.inputEmail} type="text" placeholder="Enter Email Address"></input>
               <button className={style.subscribeButton}>
                 Subscribe
-                <div className={style.arrowSubs}>
-                  <BsArrowRight />
-                </div>
-                </button>
+              </button>
             </div>
           </div>
         </Container>
       </div>
+      <Footer />
     </div>
     
   );
