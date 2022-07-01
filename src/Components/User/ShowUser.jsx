c852a4789620f6b42432ef2eda5fbce47e571f4d
baabyygalll <andiraazzahra19@gmail.com>
2022-07-01 18:47:03 +0700

    update delete user

--- src/Components/User/ShowUser.jsx
+++ src/Components/User/ShowUser.jsx
@@ -1,11 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-<<<<<<< HEAD
 import { getUsers, userSelectors, deleteUsers } from "../../features/userSlice"
-=======
-import { getUsers, userSelectors } from "../../features/userSlice"
 import { Link } from 'react-router-dom';
->>>>>>> e90fb304033d5ca52fca9fdb6fa694c1a5db9c10
 import { Button, Table } from 'react-bootstrap';
 import "./ShowUser.css"
 
@@ -38,13 +34,12 @@ const ShowUser = () => {
                   <td>{user.name}</td>
                   <td>{user.username}</td>
                   <td>
-<<<<<<< HEAD
+
                     <Button variant="primary mx-3">Edit</Button>
                     <Button onClick={()=> dispatch(deleteUsers(user.id))} variant="danger">Delete</Button>
-=======
+
                     <Link to={`edit/${user.id}`} ><Button variant="primary mx-3">Edit</Button></Link>
                     <Button variant="danger">Delete</Button>
->>>>>>> e90fb304033d5ca52fca9fdb6fa694c1a5db9c10
                   </td>
                 </tr>
               ))
