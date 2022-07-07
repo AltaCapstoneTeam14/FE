a31b01f5567c49afd6df8c8ad470c36571f73c2a
alfiarisandi <alfyarissandy@gmail.com>
2022-07-07 22:19:58 +0700

    nambang loading di login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -9,6 +9,7 @@ import { Carousel, Form } from 'react-bootstrap'
 import { Link, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import authService from '../../Services/auth-services'
+import Loading from '../../Components/Loading/Loading'
 
 function Login() {
 
@@ -19,12 +20,14 @@ function Login() {
   }
 
   const [Login, setLogin] = useState(dataLogin)
+  const [loading, setLoading] = useState(false)
 
   const [error, setError] = useState([])
   const navigate = useNavigate();
 
   const Auth = async () => {
     try{
+      setLoading(true)
       await authService.login(Login.email,Login.password).then(
         () => {
           navigate("/TopUpPage");
@@ -38,6 +41,7 @@ function Login() {
           
         }
       )
+      setLoading(false)
     }catch (err) {
       console.log(err.response)
   }
@@ -64,72 +68,81 @@ function Login() {
 
   return (
     <>
-    <div className='container-fluid'>
+    {
+      loading ? (
+        <Loading/>
+      ) : (
+        <>
+        <div className='container-fluid'>
 
-    </div>
-      <div className='d-flex flex-row w-100'>
-        <div className='left w-100 d-flex align-items-center justify-content-center'>
-          <Carousel className='login-image' controls={false} indicators={false} fade={false} interval={2000}>
-            <Carousel.Item >
-             <img src={loginImage} alt='logi' className='login-image' />
-            </Carousel.Item>
-            <Carousel.Item>
-             <img src={loginImage2} alt='login2'className='login-image' />
-            </Carousel.Item>
-            <Carousel.Item>
-             <img src={loginImage3} alt='login3' className='login-image'/>
-            </Carousel.Item>
-          </Carousel>
-          
         </div>
-        <div className='right w-100 d-flex align-items-center justify-content-center'>
-          <div className='login d-flex flex-column'>
-            <div className='d-flex flex-column mb-5'>
-              <img src={handsHello} alt='login4' style={{width : '36px'}} className='mb-3'/>
-              <span className='fs-3 fw-bold mb-4'>Welcome Back!</span>
-              <span>please login to access your account.</span>
-              {
-                error.length !== 0 && 
-                <div className='mt-4'>
-                {error.map((e, i) =>
-                    <li className='text-danger'>
-                      <span key={i} className='text-danger'>{e}</span>
-                    </li>
-                    
-                  )}
+          <div className='d-flex flex-row w-100'>
+            <div className='left w-100 d-flex align-items-center justify-content-center'>
+              <Carousel className='login-image' controls={false} indicators={false} fade={false} interval={2000}>
+                <Carousel.Item >
+                <img src={loginImage} alt='logi' className='login-image' />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img src={loginImage2} alt='login2'className='login-image' />
+                </Carousel.Item>
+                <Carousel.Item>
+                <img src={loginImage3} alt='login3' className='login-image'/>
+                </Carousel.Item>
+              </Carousel>
+              
+            </div>
+            <div className='right w-100 d-flex align-items-center justify-content-center'>
+              <div className='login d-flex flex-column'>
+                <div className='d-flex flex-column mb-5'>
+                  <img src={handsHello} alt='login4' style={{width : '36px'}} className='mb-3'/>
+                  <span className='fs-3 fw-bold mb-4'>Welcome Back!</span>
+                  <span>please login to access your account.</span>
+                  {
+                    error.length !== 0 && 
+                    <div className='mt-4'>
+                    {error.map((e, i) =>
+                        <li className='text-danger'>
+                          <span key={i} className='text-danger'>{e}</span>
+                        </li>
+                        
+                      )}
+                    </div>
+                      
+                  }
                 </div>
+
+                <Form onKeyDown={handleKeyPress}>
+                <div className='d-flex flex-column'>
+                  <div className='d-flex flex-row justify-content-between align-items-center'>
+                    <Form.Label >E-mail or phone number</Form.Label>
+                    <AiOutlineInfoCircle size='16px'/>
+                  </div>
+                  <Form.Control type="text" placeholder='Type your e-mail or phone number' className='border-0 mb-4' name = "email" value={Login.email} onChange={handleChange}/>
+
+                  <Form.Label >Password</Form.Label>
                   
-              }
-            </div>
-
-            <Form onKeyDown={handleKeyPress}>
-            <div className='d-flex flex-column'>
-              <div className='d-flex flex-row justify-content-between align-items-center'>
-                <Form.Label >E-mail or phone number</Form.Label>
-                <AiOutlineInfoCircle size='16px'/>
-              </div>
-              <Form.Control type="text" placeholder='Type your e-mail or phone number' className='border-0 mb-4' name = "email" value={Login.email} onChange={handleChange}/>
-
-              <Form.Label >Password</Form.Label>
-              
-              <div>
-                <li className='visible position-absolute list-unstyled'>
-                  <button type='button' className='button-hide' onClick={()=>setHidePassword(!hidePassword)}>
-                    {hidePassword? (<AiOutlineEyeInvisible size='16px' />) : (<AiOutlineEye size='16px'/>)}
-                  </button>
+                  <div>
+                    <li className='visible position-absolute list-unstyled'>
+                      <button type='button' className='button-hide' onClick={()=>setHidePassword(!hidePassword)}>
+                        {hidePassword? (<AiOutlineEyeInvisible size='16px' />) : (<AiOutlineEye size='16px'/>)}
+                      </button>
+                      
+                    </li>
+                    <Form.Control type={hidePassword? "password" : "text"} placeholder='Type your password' className='border-0 mb-2' name='password' value={Login.password} onChange={handleChange} />
+                  </div>
                   
-                </li>
-                <Form.Control type={hidePassword? "password" : "text"} placeholder='Type your password' className='border-0 mb-2' name='password' value={Login.password} onChange={handleChange} />
-              </div>
-              
-              <Link to='#' className='text-decoration-none mb-4'><span className='forgot'>Forgot password?</span></Link>
+                  <Link to='#' className='text-decoration-none mb-4'><span className='forgot'>Forgot password?</span></Link>
 
-              <button type='button' className='login-btn btn' onClick={Auth}><span className='fs-5'>Log In</span></button>
+                  <button type='button' className='login-btn btn' onClick={Auth}><span className='fs-5'>Log In</span></button>
+                </div>
+                </Form>
+              </div>
             </div>
-            </Form>
           </div>
-        </div>
-      </div>
+        </>
+      )
+    }
+    
     </>
   )
 }
